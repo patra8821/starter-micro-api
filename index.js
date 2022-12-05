9afbbb342a5676267d8efd786e46b55a98056af1
patra8821 <75171587+patra8821@users.noreply.github.com>
2022-12-05 09:27:28 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo! World?');
+var http = require("http");
+http
+  .createServer(function (req, res) {
+    console.log(`Just got a request at ${req.url}!`);
+    console.log(req);
+    res.write("Yo! World?");
     res.end();
-}).listen(process.env.PORT || 3000);
+  })
+  .listen(process.env.PORT || 3000);
