06419eebb1b7aa8973439da7a44fac4012194657
Your Name <you@example.com>
2022-12-05 11:05:58 +0000

    conf

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const express = require("express");
 
 const app = express();
 app.use(express.json({ extended: false, limit: "250mb" }));
-app.use(
-  express.urlencoded({
-    limit: "250mb",
-    extended: true,
-    parameterLimit: 50000,
-  })
-);
 
 app.post("*", (req, res) => {
   console.log(req);
