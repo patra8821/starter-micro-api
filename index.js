19a6cab0c5bcb4eedd69da804220b553ade57f82
Your Name <you@example.com>
2022-12-05 11:07:00 +0000

    added res

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.use(express.json({ extended: false, limit: "250mb" }));
 
 app.post("*", (req, res) => {
   console.log(req);
+  res.send("Hello World!")
 });
 
 app.listen(process.env.PORT || 3000);
