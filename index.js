d23be1a9443bc83506dd617c41940fa3adf1638a
Your Name <you@example.com>
2022-12-05 11:03:38 +0000

    express config

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 
 const app = express();
-
-app.use(express.json());
+app.use(express.json({ extended: false, limit: "250mb" }));
+app.use(
+  express.urlencoded({
+    limit: "250mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 
 app.use("*", (req, res) => {
   console.log(req);
