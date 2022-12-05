d53048cd43c2a6dac315c6845379b7fe96e51b52
Your Name <you@example.com>
2022-12-05 11:12:40 +0000

    conf

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 app.use(express.json({ extended: false, limit: "250mb" }));
 
 app.post("*", (req, res) => {
-  console.log(req);
-  res.send("Hello World!")
+  console.log(JSON.stringify(req.data));
+  res.send("Hello World!");
 });
 
 app.listen(process.env.PORT || 3000);
