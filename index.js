301112185bd693990f849ffcaf3eb36908bd86da
Your Name <you@example.com>
2022-12-06 10:56:13 +0000

    syntax fix

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ app.use(express.json({ extended: false, limit: "250mb" }));
 app.use("/roaming-stammer", checkAuth, require("./api/roamingStammer"));
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("server listing on 3000", arr);
+  console.log("server listing on 3000");
 });
