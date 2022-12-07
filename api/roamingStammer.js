de1620f5e277b9284e908ce36a0a23c86dda12c5
Your Name <you@example.com>
2022-12-07 10:34:17 +0000

    sf

--- api/roamingStammer.js
+++ api/roamingStammer.js
@@ -10,10 +10,7 @@ const roamingStammer = async (req, res) => {
     // const encryptedData = await encData(req, res, decryptedData);
     // console.log("encryptedData", encryptedData.toString("base64"));
 
-    res.status(200).json({
-      data: "success",
-      // data: "encryptedData.toString("base64")",
-    });
+    res.status(200).json({ data: "success" });
   } catch (error) {
     console.log(error);
     res.status(500).json(error.message);
