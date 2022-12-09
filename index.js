240d733d8db04c9928d10f6a904941d6368db2c0
Your Name <you@example.com>
2022-12-09 08:25:34 +0000

    testing

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 require("dotenv").config();
-const express = require("express");
 
 const connectDB = require("./config/db");
-const checkAuth = require("./middleware/access");
-
-const app = express();
-
-app.use("/roaming-stammer", checkAuth, require("./api/roamingStammer"));
 
 connectDB().then(() => {
   console.log("Db connected");
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 
-const port = process.env.PORT || 8000;
+const checkAuth = require("./middleware/access");
+
+const port = process.env.PORT;
 
 const app = express();
 app.use(cors());
 
 app.use(express.json({ extended: false, limit: "250mb" }));
+
+app.use("/roaming-stammer", checkAuth, require("./api/roamingStammer"));
 
 app.listen(port, () => {
   console.log(`server listing on port ${port}`);
