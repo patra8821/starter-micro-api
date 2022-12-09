dc08f023f102499e8fc4cf4e9bcf79c4650234eb
Your Name <you@example.com>
2022-12-09 07:35:10 +0000

    db setting with cyclic

--- config/db.js
+++ config/db.js
@@ -1,17 +1,34 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGO_DB_URL,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    err
-      ? console.error("err on connecting db", err)
-      : console.log("Successfully connected to database...");
-  },
-  () => {
-    require("./setEnvVariables")();
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      process.env.MONGO_DB_URL,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+      console.log(`MongoDB Connected: ${conn.connection.host}`)
+    );
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
   }
-);
+};
+
+connectDB().then(() => {
+  require("./setEnvVariables")();
+});
+
+// mongoose.connect(
+//   process.env.MONGO_DB_URL,
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+//   (err) => {
+//     err
+//       ? console.error("err on connecting db", err)
+//       : console.log("Successfully connected to database...");
+//   }
+// );
