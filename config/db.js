4ee52b4a9c400c7ec4c3e02565423a5125fd1216
Your Name <you@example.com>
2022-12-09 07:42:01 +0000

    testing db

--- config/db.js
+++ config/db.js
@@ -2,14 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      process.env.MONGO_DB_URL,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
-      console.log(`MongoDB Connected: ${conn.connection.host}`)
-    );
+    mongoose.connect(process.env.MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
   } catch (error) {
     console.log(error);
     process.exit(1);
@@ -17,18 +13,6 @@ const connectDB = async () => {
 };
 
 connectDB().then(() => {
+  console.log("Db connected");
   require("./setEnvVariables")();
 });
-
-// mongoose.connect(
-//   process.env.MONGO_DB_URL,
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   },
-//   (err) => {
-//     err
-//       ? console.error("err on connecting db", err)
-//       : console.log("Successfully connected to database...");
-//   }
-// );
