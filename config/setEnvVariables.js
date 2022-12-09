5772fac00769bf4963e77eda9605e5c9c1c3691f
Your Name <you@example.com>
2022-12-09 08:36:35 +0000

    testing

--- config/setEnvVariables.js
+++ config/setEnvVariables.js
@@ -3,6 +3,7 @@ const envVar = require("../models/env");
 const setEnvVariables = async () => {
   try {
     const variableENv = await envVar.find();
+    console.log(variableENv);
     variableENv.map((vars) => {
       if (process.env[`${vars.variableName}`] === vars.variableName) {
         process.env[`${vars.variableName}`] = vars.variableValue;
