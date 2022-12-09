6e3f55578336f77525f19540b32a63b846c92b4e
Your Name <you@example.com>
2022-12-09 11:33:12 +0000

    revert changes

---
+++ config/db.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+// module.exports = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_DB_URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("MongoDB Connected...");
+//   } catch (e) {
+//     console.log("/config/db.js (dev-1)", e.message); //dev-1
+//     process.exit(1);
+//   }
+// };
+
+mongoose
+  .connect(process.env.MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.trace("Db connected");
+    require("./setEnvVariables")();
+  })
+  .catch(() => {
+    console.trace("Db connection error");
+  });
+
+// c98
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 require("dotenv").config();
 
-console.trace("called");
-require("./pconfig/db")();
+require("./config/db");
